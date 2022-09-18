a6b17c7e8b2d9300ffbfbae336cacd85461e67b0
You Song Hou <yousonghou@gmail.com>
2022-09-18 19:05:26 -0400

    add batch mint to the Mint page

--- components/nonprofit/MintNFT.js
+++ components/nonprofit/MintNFT.js
@@ -1,27 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { Card, Row, Col, Form, Select, Input, Button, Typography  } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 import { Web3Storage } from 'web3.storage';
 
 const client = new Web3Storage({ token: process.env.NEXT_PUBLIC_WEB3STORAGE_APIKEY });
 
 const layout = {
-  labelCol: {
-    span: 5,
-  },
   style: { maxWidth: "700px" }
 };
 
-const tailLayout = {
-  wrapperCol: {
-    offset: 16,
-    span: 16,
-  },
-};
+const tailLayout = null;
 
 function MintNFT({ volunteerContract }) {
   const [form] = Form.useForm();
 
   const [loading, setLoading] = useState(false);
+  const [address, setAddress] = useState("");
+  const [hours, setHours] = useState("");
+  const [addressList, setAddressList] = useState([]);
+  const [hoursList, setHoursList] = useState([]);
   const [transaction, setTransaction] = useState("");
 
   const onFinish = async (values) => {
@@ -29,7 +26,7 @@ function MintNFT({ volunteerContract }) {
       setLoading(true);
       console.log(values);
 
-      const volunteerData = JSON.stringify({ charities: values.charities, volunteerAddress:  values.volunteerAddress, volunteerHours: values.volunteerHours });
+      const volunteerData = JSON.stringify({ charities: values.charities, volunteerAddress:  addressList, volunteerHours:hoursList });
       const blob = new Blob([volunteerData], {type: "text/plain"});
       const fileToUpload = new File([ blob ], 'volunteerData.json');
 
@@ -43,7 +40,7 @@ function MintNFT({ volunteerContract }) {
 
       console.log(`https://dweb.link/ipfs/${cid}`);
 
-      const transaction = await volunteerContract.mintVolunteerNFT(values.volunteerHours, values.charities, values.volunteerAddress, `https://dweb.link/ipfs/${cid}`);
+      const transaction = await volunteerContract.batchMint(addressList, hoursList, values.charities, `https://dweb.link/ipfs/${cid}`);
       const tx = await transaction.wait();
       console.log(tx);
 
@@ -59,6 +56,13 @@ function MintNFT({ volunteerContract }) {
     form.resetFields();
   };
 
+  const addToList = () => {
+    setAddressList([...addressList, address]);
+    setHoursList([...hoursList, hours]);
+    setAddress("");
+    setHours("");
+  };
+
   return (
     <div>
       <Card style={{ margin: '1rem 0'}}>
@@ -67,7 +71,7 @@ function MintNFT({ volunteerContract }) {
         </h1>
       </Card>
       <Card>
-        <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
+        <Form {...layout} form={form} name="control-hooks" onFinish={onFinish} layout= "vertical">
           <Form.Item
             name="charities"
             label="Charities"
@@ -79,28 +83,31 @@ function MintNFT({ volunteerContract }) {
           >
             <Input />
           </Form.Item>
-          <Form.Item
-            name="volunteerAddress"
-            label="Volunteer Address"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="volunteerHours"
-            label="Volunteer Hours"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
+          <Row gutter={[16, 16]} justify="space-around" align="bottom">
+            <Col className="gutter-row" span={12}>
+              <Form.Item
+                name="volunteerAddress"
+                label="Volunteer Address"
+              >
+                {addressList.map((a, index) => <p key={index}>{a}</p>)}
+                <Input value={address} onChange={(e) => setAddress(e.target.value)} />
+              </Form.Item>
+            </Col>
+            <Col className="gutter-row" span={11}> 
+              <Form.Item
+                name="volunteerHours"
+                label="Volunteer Hours"
+              >
+                {hoursList.map((h, index) => <p key={index}>{h}</p>)}
+                <Input value={hours} onChange={(e) => setHours(e.target.value)} />
+              </Form.Item>
+              
+            </Col>
+            <Col className="gutter-row" span={1}> 
+              <Button className="primary-bg-color" type="primary" icon={<PlusOutlined />} onClick={addToList} style={{ marginBottom: "1.5rem" }}/>
+            </Col>
+          </Row>
+          
 
           <Form.Item {...tailLayout}>
             <Button type="primary" htmlType="submit" className="primary-bg-color" loading={loading}>
